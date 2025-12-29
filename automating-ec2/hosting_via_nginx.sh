023d8b52f5dbd5427316f6119e2a6367e6d61eb0
Priyanshu Singh <priyanshusingh22340@gmail.com>
2025-12-29 14:12:59 +0530

    Update hosting_via_nginx.sh

--- automating-ec2/hosting_via_nginx.sh
+++ automating-ec2/hosting_via_nginx.sh
@@ -7,3 +7,8 @@ sudo apt-get install nginx -y
 echo "Hello everyone, my IP address is $(hostname -i)" | sudo tee /var/www/html/index.html
 
 sudo systemctl restart nginx
+
+
+#add this script in advanced section at the end in the code section and then launch instance
+#after launching take ip of ec2 instance and port 80 default port for nginx serverr make sure you have provided outbound access to that 
+#then http://<ec2-ip-address>:80  run this in your browser
